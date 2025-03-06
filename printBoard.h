9fad602592e299a59ea38a388025bb591f8d871d
Briannajok <143117832+Briannajok@users.noreply.github.com>
2025-03-06 13:44:04 -0700

    Create printBoard.h
    Printing the board header file

---
+++ printBoard.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <climits>
+#include <ctime>
+#include <iomanip>
+
+void print(int b[10][10], int row, int col, int db[10][10], bool end);
